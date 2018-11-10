835ba9be5c2db0c16c20b13bdf395e8c3377b600
苏登辉 <dhsu2@iflytek.com>
2018-11-10 16:47:59 +0800

    dubbo项目初始化

---
+++ dubboserver/src/main/java/com/dhsu/dubboserver/DubboserverApplication.java
@@ -0,0 +1,12 @@
+package com.dhsu.dubboserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboserverApplication.class, args);
+	}
+}
